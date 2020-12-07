47b2d34e44b6ca2f1da05ac87479bd80182e11cf
李家富 <lijiafu@show-fitness.com>
2020-12-07 18:04:06 +0800

    完善地图

--- src/views/dashboard/MerchantsNum.vue
+++ src/views/dashboard/MerchantsNum.vue
@@ -7,159 +7,163 @@
  * @FilePath: /show-admin-supervise/src/views/dashboard/BuildNum.vue
 -->
 <template>
-	<div class="numBox">
-		<div class="numItemleft">
-			<h5>商家总数量</h5>
-			<div style="margin-top: 45px;font-size: 32px;" class="numText">
-				{{ MerchantsObj.totalNum | toThousands }}
-			</div>
-		</div>
-		<div class="numItemright">
-			<div class="righttop">
-				<div class="numItem numItem1">
-					<h5>正常营业</h5>
-					<div class="numText">
-						{{ MerchantsObj.operationStatusANum | toThousands }}
-					</div>
-				</div>
-				<div class="numItem numItem2">
-					<h5>无经营场所</h5>
-					<div class="numText">
-						{{ MerchantsObj.operationStatusBNum | toThousands }}
-					</div>
-				</div>
-				<div class="numItem numItem3">
-					<h5>歇业</h5>
-					<div class="numText">
-						{{ MerchantsObj.operationStatusCNum | toThousands }}
-					</div>
-				</div>
-				<div class="numItem numItem4">
-					<h5>注销/吊销</h5>
-					<div class="numText">
-						{{ MerchantsObj.operationStatusDNum | toThousands }}
-					</div>
-				</div>
-				<div class="numItem numItem5">
-					<h5>区域选择</h5>
-					<div class="numText">
-						<el-select
-							v-model="countyCode"
-							placeholder="区域选择"
-							:multiple="true"
-							collapse-tags
-							size="small"
-                            @change="countyCodeget"
-						>
-							<el-option
-								v-for="item in countyCodeList"
-								:key="item.dictValue"
-								:label="item.dictLabel"
-								:value="item.dictValue"
-							/>
-						</el-select>
-					</div>
-				</div>
-				<div class="numItem numItem6">
-					<h5>证件数量/超期数量</h5>
-					<div class="numText">
-						{{ MerchantsObj.certificateNum }}/
-						<span style="color: red">{{
-							MerchantsObj.certificateExpiredNum
-						}}</span>
-					</div>
-				</div>
-			</div>
-			<div class="rightottom">
-				<div class="numItem numItem7">
-					<h5>正常经营</h5>
-					<div class="numText">
-						{{ MerchantsObj.businessNum | toThousands }}
-					</div>
-				</div>
-				<div class="numItem numItem8">
-					<h5>无照经营</h5>
-					<div class="numText">
-						{{ MerchantsObj.noBusinessNum | toThousands }}
-					</div>
-				</div>
-				<div class="numItem numItem9">
-					<h5>非注册地经营</h5>
-					<div class="numText">
-						{{ MerchantsObj.noAddressBusinessNum | toThousands }}
-					</div>
-				</div>
-				<div class="numItem numItem10">
-					<h5>网店</h5>
-					<div class="numText">{{ MerchantsObj.storeNum | toThousands }}</div>
-				</div>
-				<div class="numItem numItem11">
-					<h5>运输户</h5>
-					<div class="numText">
-						{{ MerchantsObj.transportNum | toThousands }}
-					</div>
-				</div>
-				<div class="numItem numItem12">
-					<h5>器具数量/超期数量</h5>
-					<div class="numText">
-						{{ MerchantsObj.applianceNum }}/
-						<span style="color: red">{{
-							MerchantsObj.applianceExpiredNum
-						}}</span>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+  <div class="numBox">
+    <div class="numItemleft">
+      <h5>商家总数量</h5>
+      <div style="margin-top: 45px;font-size: 32px;" class="numText">
+        {{ MerchantsObj.totalNum | toThousands }}
+      </div>
+    </div>
+    <div class="numItemright">
+      <div class="righttop">
+        <div class="numItem numItem1">
+          <h5>正常营业</h5>
+          <div class="numText">
+            {{ MerchantsObj.operationStatusANum | toThousands }}
+          </div>
+        </div>
+        <div class="numItem numItem2">
+          <h5>无经营场所</h5>
+          <div class="numText">
+            {{ MerchantsObj.operationStatusBNum | toThousands }}
+          </div>
+        </div>
+        <div class="numItem numItem3">
+          <h5>歇业</h5>
+          <div class="numText">
+            {{ MerchantsObj.operationStatusCNum | toThousands }}
+          </div>
+        </div>
+        <div class="numItem numItem4">
+          <h5>注销/吊销</h5>
+          <div class="numText">
+            {{ MerchantsObj.operationStatusDNum | toThousands }}
+          </div>
+        </div>
+        <div class="numItem numItem5">
+          <h5>区域选择</h5>
+          <div class="numText">
+            <el-select
+              v-model="countyCode"
+              placeholder="区域选择"
+              :multiple="true"
+              collapse-tags
+              size="small"
+              @change="countyCodeget"
+            >
+              <el-option
+                v-for="item in countyCodeList"
+                :key="item.dictValue"
+                :label="item.dictLabel"
+                :value="item.dictValue"
+              />
+            </el-select>
+          </div>
+        </div>
+        <div class="numItem numItem6">
+          <h5>证件数量/超期数量</h5>
+          <div class="numText">
+            {{ MerchantsObj.certificateNum }}/
+            <span style="color: red">{{
+              MerchantsObj.certificateExpiredNum
+            }}</span>
+          </div>
+        </div>
+      </div>
+      <div class="rightottom">
+        <div class="numItem numItem7">
+          <h5>正常经营</h5>
+          <div class="numText">
+            {{ MerchantsObj.businessNum | toThousands }}
+          </div>
+        </div>
+        <div class="numItem numItem8">
+          <h5>无照经营</h5>
+          <div class="numText">
+            {{ MerchantsObj.noBusinessNum | toThousands }}
+          </div>
+        </div>
+        <div class="numItem numItem9">
+          <h5>非注册地经营</h5>
+          <div class="numText">
+            {{ MerchantsObj.noAddressBusinessNum | toThousands }}
+          </div>
+        </div>
+        <div class="numItem numItem10">
+          <h5>网店</h5>
+          <div class="numText">{{ MerchantsObj.storeNum | toThousands }}</div>
+        </div>
+        <div class="numItem numItem11">
+          <h5>运输户</h5>
+          <div class="numText">
+            {{ MerchantsObj.transportNum | toThousands }}
+          </div>
+        </div>
+        <div class="numItem numItem12">
+          <h5>器具数量/超期数量</h5>
+          <div class="numText">
+            {{ MerchantsObj.applianceNum }}/
+            <span style="color: red">{{
+              MerchantsObj.applianceExpiredNum
+            }}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import { getNum } from "@/api/index/index";
+import { getNum } from '@/api/index/index'
 
 export default {
-	data() {
-		return {
-            countyCode: undefined,
-            countyCodeList: [],
-			MerchantsObj: {},
-		};
-	},
-	filters: {
-		toThousands: function (value) {
-			return (value || 0).toString().replace(/(\d)(?=(?:\d{3})+$)/g, "$1,");
-		},
-	},
-	created() {
-        this.getDicts('map_county_code').then(response => {
-            this.countyCodeList = response.data
-			this.countyCode = [response.data[0].dictValue]
-	        this.getData();
-			this.$emit("MerchantsId", [response.data[0].dictValue]);
-        })
-	},
-	methods: {
-        countyCodeget (val) {
-			console.log(val.length);
-			if (!val.length) {
-				this.countyCode = [this.countyCodeList[0].dictValue]
-				this.getData();
-				this.$emit("MerchantsId", this.countyCode);
-				return
-			}
-			this.$emit("MerchantsId", val);
-        },
-		getData() {
-			getNum({ codes: this.countyCode }).then((response) => {
-				console.log(response);
-				if (response.code === 200) {
-					this.MerchantsObj = response.data;
-				} else {
-					this.msgError(response.msg);
-				}
-			});
-		},
-	},
-};
+  filters: {
+    toThousands: function(value) {
+      return (value || 0).toString().replace(/(\d)(?=(?:\d{3})+$)/g, '$1,')
+    }
+  },
+  data() {
+    return {
+      countyCode: undefined,
+      countyCodeList: [],
+      MerchantsObj: {}
+    }
+  },
+  created() {
+    this.getDicts('map_county_code').then(response => {
+      this.countyCodeList = response.data
+      // this.countyCode = [response.data[0].dictValue]
+	        this.getData()
+      // this.$emit('MerchantsId', [response.data[0].dictValue])
+
+      this.$emit('MerchantsId', this.countyCode)
+    })
+  },
+  methods: {
+    countyCodeget(val) {
+      // if (!val.length) {
+      //   // this.countyCode = [this.countyCodeList[0].dictValue]
+      //   this.countyCode = []
+      //   this.getData()
+      //   this.$emit('MerchantsId', this.countyCode)
+      //   return
+      // }
+      this.countyCode = val
+      this.getData()
+      this.$emit('MerchantsId', val)
+    },
+    getData() {
+      getNum({ codes: this.countyCode }).then((response) => {
+        console.log()
+        if (response.code === 200) {
+          this.MerchantsObj = response.data
+        } else {
+          this.msgError(response.msg)
+        }
+      })
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
